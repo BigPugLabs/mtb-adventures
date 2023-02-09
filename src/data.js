82e6cd83da43785ada0a06212c1761f258d7a923
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-02-09 16:17:25 +0000

    fix css clipped descenders

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default [
     {
         id: 1,
-        title: "Jumping in the woods",
+        title: "Jumping in the woods in the woods in the woods",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 36,
         coverImg: "devon-hawkins-sm3.jpg",
